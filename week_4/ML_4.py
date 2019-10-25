0d24fc3092f0dea6e691e7a0f61799dd2054bf7e
1995hyz <1995hyz@gmail.com>
2019-10-25 01:09:31 -0400

    Add Midterm

--- week_4/ML_4.py
+++ week_4/ML_4.py
@@ -1,129 +1,96 @@
 from __future__ import print_function
 from tensorflow import keras
-"""from keras.datasets import cifar10
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D"""
-import os
+import matplotlib.pyplot as plt
 
-batch_size = 16
+batch_size = 32
 num_classes = 10
 epochs = 100
-data_augmentation = True
 num_predictions = 20
-save_dir = os.path.join(os.getcwd(), 'saved_models')
-model_name = 'keras_cifar10_trained_model.h5'
 
-# The data, split between train and test sets:
-(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_validation = x_train[40000:]
+    y_validation = y_train[40000:]
+    x_train = x_train[:40000]
+    y_train = y_train[:40000]
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
 
-# Convert class vectors to binary class matrices.
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    # Convert class vectors to binary class matrices.
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_validation = keras.utils.to_categorical(y_validation, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = keras.Sequential()
-model.add(keras.layers.Conv2D(32, (3, 3), padding='same',
-                 input_shape=x_train.shape[1:]))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Activation('relu'))
-model.add(keras.layers.Conv2D(32, (3, 3)))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Activation('relu'))
-model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(keras.layers.Dropout(0.25))
+    model = keras.Sequential()
+    model.add(keras.layers.Conv2D(32, (3, 3), padding='same',
+                     input_shape=x_train.shape[1:]))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(32, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Dropout(0.25))
 
-model.add(keras.layers.Conv2D(64, (3, 3), padding='same'))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Activation('relu'))
-model.add(keras.layers.Conv2D(64, (3, 3)))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Activation('relu'))
-model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(keras.layers.Dropout(0.25))
+    model.add(keras.layers.Conv2D(64, (3, 3), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Conv2D(64, (3, 3)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(keras.layers.Dropout(0.25))
 
-model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(512))
-model.add(keras.layers.Activation('relu'))
-model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(num_classes))
-model.add(keras.layers.Activation('softmax'))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(512))
+    model.add(keras.layers.Activation('relu'))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.Dense(num_classes))
+    model.add(keras.layers.Activation('softmax'))
 
-# initiate RMSprop optimizer
-# opt = keras.optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
-opt = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+                  metrics=['accuracy'])
 
-# Let's train the model using RMSprop
-model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-
-if not data_augmentation:
-    print('Not using data augmentation.')
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              validation_data=(x_test, y_test),
-              shuffle=True)
-else:
-    print('Using real-time data augmentation.')
-    # This will do preprocessing and realtime data augmentation:
-    datagen = keras.preprocessing.image.ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        zca_epsilon=1e-06,  # epsilon for ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        # randomly shift images horizontally (fraction of total width)
-        width_shift_range=0.1,
-        # randomly shift images vertically (fraction of total height)
-        height_shift_range=0.1,
-        shear_range=0.,  # set range for random shear
-        zoom_range=0.,  # set range for random zoom
-        channel_shift_range=0.,  # set range for random channel shifts
-        # set mode for filling points outside the input boundaries
-        fill_mode='nearest',
-        cval=0.,  # value used for fill_mode = "constant"
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False,  # randomly flip images
-        # set rescaling factor (applied before any other transformation)
-        rescale=None,
-        # set function that will be applied on each input
-        preprocessing_function=None,
-        # image data format, either "channels_first" or "channels_last"
-        data_format=None,
-        # fraction of images reserved for validation (strictly between 0 and 1)
-        validation_split=0.0)
-
-    # Compute quantities required for feature-wise normalization
-    # (std, mean, and principal components if ZCA whitening is applied).
+    # Do preprocessing and realtime data augmentation:
+    datagen = keras.preprocessing.image.ImageDataGenerator(width_shift_range=0.1,
+                                                           height_shift_range=0.1,
+                                                           horizontal_flip=True,
+                                                           vertical_flip=False
+                                                           )
     datagen.fit(x_train)
 
-    # Fit the model on the batches generated by datagen.flow().
-    model.fit_generator(datagen.flow(x_train, y_train,
-                                     batch_size=batch_size),
-                        epochs=epochs,
-                        validation_data=(x_test, y_test),
-                        workers=1)
+    history = model.fit_generator(datagen.flow(x_train, y_train,
+                                               batch_size=batch_size),
+                                  epochs=epochs,
+                                  validation_data=(x_validation, y_validation),
+                                  workers=1)
 
-# Save model and weights
-if not os.path.isdir(save_dir):
-    os.makedirs(save_dir)
-model_path = os.path.join(save_dir, model_name)
-model.save(model_path)
-print('Saved trained model at %s ' % model_path)
+    # Score trained model.
+    scores = model.evaluate(x_test, y_test, verbose=1)
+    print('Test loss:', scores[0])
+    print('Test accuracy:', scores[1])
 
-# Score trained model.
-scores = model.evaluate(x_test, y_test, verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+    # Plot training accuracy
+    plt.plot(history.history["accuracy"])
+    plt.plot(history.history["val_accuracy"])
+    plt.title("Model Accuracy")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
+
+    # Plot history for loss
+    plt.plot(history.history["loss"])
+    plt.plot(history.history["val_loss"])
+    plt.title("Model Loss")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
