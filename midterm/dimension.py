3fd0a6b07a5cebf74d88ebc2bdee8f363c5fa6de
1995hyz <1995hyz@gmail.com>
2019-10-25 15:53:44 -0400

    Improve Midterm

--- midterm/dimension.py
+++ midterm/dimension.py
@@ -5,10 +5,10 @@ from tqdm import trange
 import matplotlib.pyplot as plt
 
 NUM_CLASSES = 10
-BATCH_SIZE = 2048
-NUM_BATCHES = 4096
-LEARNING_RATE = 10**-2
-MOMENTUM = .97
+BATCH_SIZE = 16384#2048
+NUM_BATCHES = 20000
+LEARNING_RATE = 10**-3
+#MOMENTUM = .97
 
 
 class Data(object):
@@ -30,19 +30,20 @@ class Data(object):
         choices = np.random.choice(self.index, size=batch_size)
         return self.x_train[choices], self.y_train[choices]
 
-    def get_test(self, batch_size=BATCH_SIZE):
-        test_index = np.arrange(start=0, stop=batch_size)
-        val_index = np.arrange(start=batch_size, stop=batch_size*2)
-        test_and_validate = (self.x_test[test_index], self.y_test[test_index], self.x_test[val_index], self.y_test[val_index])
-        return test_and_validate
+    def get_test(self):
+        choices = np.random.choice(10000, size=10000)
+        test_index = choices[:5000]
+        val_index = choices[5000:]
+        return self.x_test[test_index], self.y_test[test_index], self.x_test[val_index], self.y_test[val_index]
 
 
 def matrix_norm(matrix_in):
-    n_columns = np.shape(matrix_in)[1]#Extracts number of columns
+    n_columns = np.shape(matrix_in)[1]  # Extracts number of columns
     matrix_out = np.zeros(np.shape(matrix_in))
     for i in range(n_columns):
         matrix_out[:, i] = matrix_in[:, i]/np.linalg.norm(matrix_in[:, i])
     return matrix_out
+
 
 class Model(tf.Module):
     def __init__(self, dimensionality):
@@ -50,20 +51,22 @@ class Model(tf.Module):
         self.P = matrix_norm(tf.random.normal(shape=[199210, dimensionality]))   # Projects lower dimensional adaptive wight into higher dimensional parameter-space
         # We may need to add orthogonalization to self.P
         self.theta_d = tf.Variable(tf.zeros(shape=[dimensionality, 1]))     # This lower dimensional weight vector is the only learned parameter
+        self.Theta_D = 0
 
-    def __call__(self, image):
-        # image is 784 x 1
-        Theta_D = self.theta_0_D + self.P @ self.theta_d    # Projects into parameter space
+    def __call__(self, image, theta_flag=1):
+        if theta_flag:
+            # image is 784 x 1
+            self.Theta_D = self.theta_0_D + self.P @ self.theta_d    # Projects into parameter space
         # We must now reshape Theta_D into the right weight and bias shapes. Order doesn't matter as
         # it'll just learn to the right places as long as order is consistent
-        w1 = tf.reshape(Theta_D[0:156800], shape=[200, 784])     # Takes first 200*784 parameters and shapes it into weight
-        b1 = tf.reshape(Theta_D[156800:157000], shape=[200, 1])    # Takes next 200*1 parameters and shapes it into bias
+        w1 = tf.reshape(self.Theta_D[0:156800], shape=[200, 784])     # Takes first 200*784 parameters and shapes it into weight
+        b1 = tf.reshape(self.Theta_D[156800:157000], shape=[200, 1])    # Takes next 200*1 parameters and shapes it into bias
 
-        w2 = tf.reshape(Theta_D[157000:197000], shape=[200, 200])  # Takes next 200*200 parameters and shapes it into weight
-        b2 = tf.reshape(Theta_D[197000:197200], shape=[200, 1])  # Takes next 200*1 parameters and shapes it into bias
+        w2 = tf.reshape(self.Theta_D[157000:197000], shape=[200, 200])  # Takes next 200*200 parameters and shapes it into weight
+        b2 = tf.reshape(self.Theta_D[197000:197200], shape=[200, 1])  # Takes next 200*1 parameters and shapes it into bias
 
-        w3 = tf.reshape(Theta_D[197200:199200], shape=[10, 200])  # Takes next 10*200 parameters and shapes it into weight
-        b3 = tf.reshape(Theta_D[199200:], shape=[10, 1])  # Takes next 10*1 parameters and shapes it into bias
+        w3 = tf.reshape(self.Theta_D[197200:199200], shape=[10, 200])  # Takes next 10*200 parameters and shapes it into weight
+        b3 = tf.reshape(self.Theta_D[199200:], shape=[10, 1])  # Takes next 10*1 parameters and shapes it into bias
         # The paper does not specify the non-linearities used. For now lets stick with relu
         self.layer1 = tf.nn.relu(w1 @ image + b1)
         self.layer2 = tf.nn.relu(w2 @ self.layer1 + b2)
@@ -74,10 +77,14 @@ class Model(tf.Module):
 if __name__ == "__main__":
     data = Data()
     model = Model(dimensionality=1500)
-    optimizer = tf.optimizers.SGD(learning_rate=LEARNING_RATE, momentum=MOMENTUM)
+    optimizer = tf.optimizers.Adam(learning_rate=LEARNING_RATE)
 
     loss_track = np.full((NUM_BATCHES, 1), np.nan)
     accuracy_track = np.full((NUM_BATCHES, 1), np.nan)
+    loss_track_test = np.full((NUM_BATCHES, 1), np.nan)
+    accuracy_track_test = np.full((NUM_BATCHES, 1), np.nan)
+
+    x_test, y_test, x_val, y_val = data.get_test()
 
     # Training
     bar = trange(NUM_BATCHES)
@@ -96,12 +103,25 @@ if __name__ == "__main__":
         grads = tape.gradient(loss, model.variables)
         optimizer.apply_gradients(zip(grads, model.variables))
 
-        bar.set_description(f"Loss @ {i} => {loss.numpy():0.6f}; Accuracy => {accuracy:0.6f}")
+        p_test = model(x_test, 0)
+        loss_test = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf.squeeze(y_test), logits=p_test))
+        choice_test = np.argmax(p_test, axis=1)
+        correct_choice_test = np.argmax(y_test, axis=1)
+        accuracy_test = sum(choice_test == correct_choice_test) / 5000
+        loss_track_test[i] = loss_test
+        accuracy_track_test[i] = accuracy_test
+
+        bar.set_description(f"Loss @ {i} => {loss.numpy():0.6f}, Accuracy => {accuracy:0.6f};   "
+                            f""
+                            f"Test Loss => {loss_test.numpy():0.6f}, Accuracy => {accuracy_test:0.6f}")
         bar.refresh()
 
     plt.plot(loss_track)
+    plt.plot(loss_track_test)
     plt.title("Loss")
     plt.show()
+
     plt.plot(accuracy_track)
+    plt.plot(accuracy_track_test)
     plt.title("Accuracy")
     plt.show()
